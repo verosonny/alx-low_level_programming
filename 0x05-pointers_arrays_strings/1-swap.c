e649cbc9eb1d8c9faa5200634be706da0782dec9
Verosonny <Verosonny12@gmail.com>
2023-09-18 23:35:17 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,8 +9,12 @@
 void swap_int(int *a, int *b)
 {
 int a;
+int b;
 
-a = *a;
-b = *b;
-*b = a;
+a = 98;
+b = 42;
+printf("a=%d, b=%d\n", a, b);
+swap_int(&a, &b);
+printf("a=%d, b=%d\n", a, b);
+return (0);
 }
